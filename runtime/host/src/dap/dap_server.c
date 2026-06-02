08e880a184beb5c1b1554a6e7dfdbf1c2dd363d0
Tom Dunstan <github@tomd.cc>
2026-06-02 17:19:54 +0930

    fix(dap): sort poll.h include alphabetically for clang-format

--- runtime/host/src/dap/dap_server.c
+++ runtime/host/src/dap/dap_server.c
@@ -17,12 +17,12 @@
 #define _GNU_SOURCE
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
